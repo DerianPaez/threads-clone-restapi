fd88e00ac5930b60be13ba451660eee6ca75d254
james.mendoza <james.mendoza@rappi.com>
2023-08-31 17:58:46 -0500

    Allow all domains

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "*" }));
 
 app.get("/ping", (req, res) => res.send("pong"));
 
